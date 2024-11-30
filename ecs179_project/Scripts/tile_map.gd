04079a9bc0fb76d9914395b3ee91ae87170a58a3
XMNNguyen <rupyman95@gmail.com>
2024-11-29 18:43:31 -0800

    cleaned up map and classsified tiles

--- ecs179_project/Scripts/tile_map.gd
+++ ecs179_project/Scripts/tile_map.gd
@@ -9,12 +9,44 @@ enum layers{
 }
 
 
-# TODO: set up a couple more invisible collision tiles for tiles 1 layer above
-var boundery := Vector2i(9, 0)
 
-# TODO: fill out the slopes table of all the atlas coords of slopes
+var boundery := Vector2i(9, 0)
+var wall_bounderies:Dictionary = {
+								Vector2(-1, 0) : Vector2(9, 1),
+								Vector2(1, 0) : Vector2(8, 1),
+								Vector2(0, -1) : Vector2(8, 2),
+								Vector2(0, 1) : Vector2(8, 0),
+							  } 
+
 # atlas coords of all the slopes in our tile set
-var slopes:Array[Vector2] = []
+var slopes:Array[Vector2] = [
+							Vector2(0, 3), Vector2(1, 3), Vector2(2, 3), Vector2(3, 3), 
+							Vector2 (5, 3), Vector2(6, 3), Vector2(7, 3), Vector2(8, 3), 
+							Vector2(0, 4), Vector2(1, 4), Vector2(2, 4), Vector2(3, 4),
+							]
+
+# atlas coords of all the blocks in our tile set
+var blocks:Array[Vector2] = [
+							Vector2(4, 3), Vector2(4, 5), Vector2(5, 5), Vector2(0, 6),
+							Vector2(1, 6), Vector2(2, 6), Vector2(3, 6), Vector2(4, 6),
+							Vector2(5, 6), Vector2(1, 7), Vector2(2, 7), Vector2(3, 7),
+							Vector2(4, 7), Vector2(5, 7), Vector2(6, 7), Vector2(7, 7),
+							Vector2(8, 9), Vector2(9, 9),
+							]
+
+var other:Array[Vector2] = [
+							Vector2(0, 5), Vector2(1, 5), Vector2(2, 5), Vector2(3, 5),
+							Vector2(6, 5), Vector2(7, 5), Vector2(8, 5), Vector2(9, 5),
+							Vector2(6, 6), Vector2(7, 6), Vector2(8, 6), Vector2(9, 6),
+							Vector2(0, 7), Vector2(0, 11), Vector2(1, 11), Vector2(2, 11),
+							Vector2(3, 11), Vector2(4, 11), Vector2(5, 11), Vector2(6, 11),
+							Vector2(7, 11), Vector2(8, 11), Vector2(9, 11), Vector2(0, 12),
+							Vector2(1, 12), Vector2(2, 12), Vector2(3, 12), Vector2(4, 12),
+							Vector2(5, 12), Vector2(6, 12), Vector2(7, 12), Vector2(8, 12),
+							Vector2(9, 12), Vector2(0, 13), Vector2(1, 13), Vector2(2, 14),
+							Vector2(3, 14), Vector2(4, 13), Vector2(5, 13),
+							]
+							
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -34,11 +66,21 @@ func setup_boundery(current_layer: layers, source: int):
 	# go through every tile in the layer
 	# check the surrounding tiles around the currrent tile
 	for tile in layer:
-		for offset in offset_direction:
-			# if the tile we are checking is empty, place a boundery tile
-			if get_cell_source_id(current_layer, tile + offset) == -1:
-				set_cell(current_layer, tile + offset, source, boundery)
-				
+		# only place boundery tiles if the tile is not a slope
+		if get_cell_atlas_coords(current_layer, tile) not in slopes:
+			for offset in offset_direction:
+				# if the tile we are checking is empty, place a boundery tile
+				if get_cell_source_id(current_layer, tile + offset) == -1:
+					set_cell(current_layer, tile + offset, source, boundery)
+	
 	# TODO: do one more set of checks on the layer above currrent_layer and add the corresponding collision bounderies for the walls
+	var layer_above:Array[Vector2i] = get_used_cells(current_layer + 1)
+	for tile in layer_above:
+		# only place boundery tiles if the tile is not a slope
+		if (get_cell_atlas_coords(current_layer + 1, tile) not in slopes and
+			get_cell_atlas_coords(current_layer + 1, tile) not in other):
+			for offset in offset_direction:
+				if get_cell_source_id(current_layer + 1, tile + offset) == -1:
+					set_cell(current_layer + 1, tile + offset, source, wall_bounderies[Vector2(offset)])
 				
 			
